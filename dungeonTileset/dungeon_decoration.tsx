2e98c2dc21c7007d2145ef680576fdf22e280e13
Jan Staffel <jan.staffel@gmail.com>
2021-01-14 23:10:49 +0100

    improved apicellar

---
+++ dungeonTileset/dungeon_decoration.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dungeon_decoration" tilewidth="16" tileheight="16" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="13" height="14" source="candleB_02.png"/>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="13" height="14" source="candleB_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="13" height="12" source="spike_0.png"/>
+ </tile>
+ <tile id="4">
+  <image width="13" height="12" source="spike_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="13" height="12" source="spike_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="13" height="13" source="spike_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="13" height="14" source="spike_4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="torch_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="7" height="15" source="candleA_02.png"/>
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <image width="7" height="16" source="candleA_03.png"/>
+ </tile>
+ <tile id="11">
+  <image width="7" height="14" source="candleA_04.png"/>
+ </tile>
+</tileset>
